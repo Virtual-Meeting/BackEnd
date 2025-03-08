eff95df90f76beaef8efe871fa46fb6b23bca0fa
minjung <minjung0768@gmail.com>
2025-03-08 13:44:06 +0900

    Refactor: KurentoChatApplication 리팩토링
    필요 없는 어노테이션 삭제

--- src/main/java/org/improvejava/kurento_chat/KurentoChatApplication.java
+++ src/main/java/org/improvejava/kurento_chat/KurentoChatApplication.java
@@ -2,12 +2,10 @@ package org.improvejava.kurento_chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @SpringBootApplication
-@EnableWebSocket
 public class KurentoChatApplication {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(KurentoChatApplication.class, args);
     }
 }
