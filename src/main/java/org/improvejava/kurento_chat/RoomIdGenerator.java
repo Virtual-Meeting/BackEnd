eb6bc8bc9ebf4e63309e707aa8bc9d557ef3abad
minjung <minjung0768@gmail.com>
2025-03-08 12:07:21 +0900

    fix: generateRoomId 에러 처리 추가
    생성된 방의 수가 1,000,000개가 넘어갈 경우 발생하는 에러 처리

--- src/main/java/org/improvejava/kurento_chat/RoomIdGenerator.java
+++ src/main/java/org/improvejava/kurento_chat/RoomIdGenerator.java
@@ -8,16 +8,20 @@ import java.util.Set;
 
 @Component
 public class RoomIdGenerator {
-    private static final Set<String> generatedIds = new HashSet<>();
+    private static final Set<String> generatedRoomIds = new HashSet<>();
     private static final Random random = new Random();
 
     public static synchronized String generateRoomId() {
+        if (generatedRoomIds.size() == 1_000_000) {
+            throw new IllegalStateException("더 이상 새로운 방 ID를 생성할 수 없습니다.");
+        }
+
         String roomId;
         do {
             roomId = String.format("%06d", random.nextInt(1_000_000)); // 000000 ~ 999999
-        } while (generatedIds.contains(roomId)); // 중복 체크
+        } while (generatedRoomIds.contains(roomId)); // 중복 체크
 
-        generatedIds.add(roomId);
+        generatedRoomIds.add(roomId); // 중복 체크 통과한 roomId 리스트에 추가
         return roomId;
     }
 }
